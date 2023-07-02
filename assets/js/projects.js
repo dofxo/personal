8a3ac6ae69329a8608ad28eedd78beb32c37e517
dofxo <kargat504@gmail.com>
2023-07-02 23:16:14 +0330

    fix(projects/button): hover style

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -3,14 +3,14 @@ const projectMaker = ({ imageUrl, projectName, githubLink, projectLink }) => {
 	<div class="project">
 		<img src="assets/images/${imageUrl}">
 		<div class="projects-details">
-			<h3 class="project-title dark-text">${projectName}</h3>
+			<h3 class="project-title">${projectName}</h3>
 
 			<div id="buttons">
 	<a href="${githubLink}" target="_blank">
-		<button class="view-project  dark-bg">View Project</button>
+		<button class="view-project">View Project</button>
 		</a>
 		<a href="${projectLink}/" target="_blank">
-			<button class="visit-website  dark-bg">Visit Website</button>
+			<button class="visit-website">Visit Website</button>
 		</a>
 </div>
 	</div>
